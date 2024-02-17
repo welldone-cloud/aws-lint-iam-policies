44258e167b0dd872cddeb9ca86e12b2a5795b09a
michael-kirchner-at <michael.kirchner@welldone.cloud>
2024-02-17 12:17:34 +0100

    Fixed error when Redshift serverless is not available in a region

--- policytypes/redshift_serverless_snapshot_policies.py
+++ policytypes/redshift_serverless_snapshot_policies.py
@@ -3,21 +3,28 @@ def analyze(account_id, region, boto_session, boto_config, policy_analysis_funct
     list_snapshots_paginator = redshift_client.get_paginator("list_snapshots")
 
     # Iterate all Redshift serverless snapshots
-    for snapshots_page in list_snapshots_paginator.paginate():
-        for snapshot in snapshots_page["snapshots"]:
-            # Fetch the snapshot policy
-            try:
-                get_resource_policy_response = redshift_client.get_resource_policy(resourceArn=snapshot["snapshotArn"])
-            except redshift_client.exceptions.from_code("ResourceNotFoundException"):
-                continue
+    try:
+        for snapshots_page in list_snapshots_paginator.paginate():
+            for snapshot in snapshots_page["snapshots"]:
+                # Fetch the snapshot policy
+                try:
+                    get_resource_policy_response = redshift_client.get_resource_policy(
+                        resourceArn=snapshot["snapshotArn"]
+                    )
+                except redshift_client.exceptions.from_code("ResourceNotFoundException"):
+                    continue
 
-            policy_analysis_function(
-                account_id=account_id,
-                region=region,
-                boto_session=boto_session,
-                resource_type="AWS::RedshiftServerless::Snapshot",
-                resource_name=snapshot["snapshotName"],
-                resource_arn=snapshot["snapshotArn"],
-                policy_document=get_resource_policy_response["resourcePolicy"]["policy"],
-                policy_type="RESOURCE_POLICY",
-            )
+                policy_analysis_function(
+                    account_id=account_id,
+                    region=region,
+                    boto_session=boto_session,
+                    resource_type="AWS::RedshiftServerless::Snapshot",
+                    resource_name=snapshot["snapshotName"],
+                    resource_arn=snapshot["snapshotArn"],
+                    policy_document=get_resource_policy_response["resourcePolicy"]["policy"],
+                    policy_type="RESOURCE_POLICY",
+                )
+
+    # Redshift serverless is not available in this region
+    except redshift_client.exceptions.from_code("ValidationException"):
+        pass
