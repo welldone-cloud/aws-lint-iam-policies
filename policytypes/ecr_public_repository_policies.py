4f9fa12132df4d690492ebe73c64f6bfda8efb2a
michael-kirchner-at <michael.kirchner@welldone.cloud>
2024-04-07 10:32:29 +0200

    Fixed issue that ECR public repository policies don't need a "Resource" element

--- policytypes/ecr_public_repository_policies.py
+++ policytypes/ecr_public_repository_policies.py
@@ -22,4 +22,5 @@ def analyze(account_id, region, boto_session, boto_config, policy_analysis_funct
                 resource_arn=repository["repositoryArn"],
                 policy_document=get_repository_policy_response["policyText"],
                 policy_type="RESOURCE_POLICY",
+                ignore_finding_issue_codes=["MISSING_RESOURCE"],
             )
