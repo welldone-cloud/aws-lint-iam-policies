43c0c678bf59cc1e3fb55529f523421259e101d0
michael-kirchner-at <michael.kirchner@welldone.cloud>
2024-06-16 16:37:09 +0200

    Fixed error with account instances of Identity Center

--- policytypes/iam_identity_center_permission_set_inline_policies.py
+++ policytypes/iam_identity_center_permission_set_inline_policies.py
@@ -17,33 +17,38 @@ def analyze(account_id, region, boto_session, boto_config, policy_analysis_funct
             # Iterate all permission sets
             instance_arn = instance["InstanceArn"]
             permission_set_paginator = sso_client.get_paginator("list_permission_sets")
-            for permission_set_page in permission_set_paginator.paginate(InstanceArn=instance_arn):
-                for permission_set_arn in permission_set_page["PermissionSets"]:
+            try:
+                for permission_set_page in permission_set_paginator.paginate(InstanceArn=instance_arn):
+                    for permission_set_arn in permission_set_page["PermissionSets"]:
 
-                    # Get inline policy
-                    get_inline_policy_response = sso_client.get_inline_policy_for_permission_set(
-                        InstanceArn=instance_arn,
-                        PermissionSetArn=permission_set_arn,
-                    )
+                        # Get inline policy
+                        get_inline_policy_response = sso_client.get_inline_policy_for_permission_set(
+                            InstanceArn=instance_arn,
+                            PermissionSetArn=permission_set_arn,
+                        )
 
-                    # Skip if no inline policy is set
-                    if not get_inline_policy_response["InlinePolicy"]:
-                        continue
+                        # Skip if no inline policy is set
+                        if not get_inline_policy_response["InlinePolicy"]:
+                            continue
 
-                    # Get permission set details
-                    describe_permission_response = sso_client.describe_permission_set(
-                        InstanceArn=instance_arn,
-                        PermissionSetArn=permission_set_arn,
-                    )
+                        # Get permission set details
+                        describe_permission_response = sso_client.describe_permission_set(
+                            InstanceArn=instance_arn,
+                            PermissionSetArn=permission_set_arn,
+                        )
 
-                    policy_analysis_function(
-                        account_id=account_id,
-                        region=region,
-                        boto_session=boto_session,
-                        source_service=SOURCE_SERVICE,
-                        resource_type="AWS::SSO::PermissionSet",
-                        resource_name=describe_permission_response["PermissionSet"]["Name"],
-                        resource_arn=permission_set_arn,
-                        policy_document=get_inline_policy_response["InlinePolicy"],
-                        policy_type="IDENTITY_POLICY",
-                    )
+                        policy_analysis_function(
+                            account_id=account_id,
+                            region=region,
+                            boto_session=boto_session,
+                            source_service=SOURCE_SERVICE,
+                            resource_type="AWS::SSO::PermissionSet",
+                            resource_name=describe_permission_response["PermissionSet"]["Name"],
+                            resource_arn=permission_set_arn,
+                            policy_document=get_inline_policy_response["InlinePolicy"],
+                            policy_type="IDENTITY_POLICY",
+                        )
+
+            except sso_client.exceptions.from_code("ValidationException"):
+                # Account instances of Identity Center don't support permission sets, thus continue here
+                continue
