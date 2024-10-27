e6b086ad92dd8ce8e7bf3a326617d4cbcca4a591
michael-kirchner-at <michael.kirchner@welldone.cloud>
2024-10-27 18:59:18 +0100

    Fixed duplicate results bug in case a single resource is shared via multiple RAM resource shares

--- modules/policy_types/_ram_based_policies.py
+++ modules/policy_types/_ram_based_policies.py
@@ -17,26 +17,30 @@ def analyze(
     ram_client = boto_session.client(SOURCE_SERVICE, config=boto_config, region_name=region)
     resources_paginator = ram_client.get_paginator("list_resources")
 
-    # Iterate resources via RAM
+    # Iterate resources via RAM. A resource ARN may be returned multiple times if the same resource is shared
+    # via multiple RAM resource shares. Thus first create a list of unique ARNs.
+    resource_arns = set()
     for resource_page in resources_paginator.paginate(resourceType=ram_resource_type, resourceOwner="SELF"):
         for resource in resource_page["resources"]:
+            resource_arns.add(resource["arn"])
 
-            # Iterate attached policies
-            policies_paginator = ram_client.get_paginator("get_resource_policies")
-            for policies_page in policies_paginator.paginate(resourceArns=[resource["arn"]]):
-                for policy in policies_page["policies"]:
+    # Iterate policies for all collected resource ARNs
+    for resource_arn in resource_arns:
+        policies_paginator = ram_client.get_paginator("get_resource_policies")
+        for policies_page in policies_paginator.paginate(resourceArns=[resource_arn]):
+            for policy in policies_page["policies"]:
 
-                    # Skip if policy is empty
-                    if not json.loads(policy):
-                        continue
+                # Skip if policy is empty
+                if not json.loads(policy):
+                    continue
 
-                    policy_analysis_function(
-                        account_id=account_id,
-                        region=region,
-                        source_service=SOURCE_SERVICE,
-                        resource_type=cfn_resource_type,
-                        resource_name=resource["arn"].split(arn_to_name_split_char)[-1],
-                        resource_arn=resource["arn"],
-                        policy_document=policy,
-                        policy_type="RESOURCE_POLICY",
-                    )
+                policy_analysis_function(
+                    account_id=account_id,
+                    region=region,
+                    source_service=SOURCE_SERVICE,
+                    resource_type=cfn_resource_type,
+                    resource_name=resource_arn.split(arn_to_name_split_char)[-1],
+                    resource_arn=resource_arn,
+                    policy_document=policy,
+                    policy_type="RESOURCE_POLICY",
+                )
