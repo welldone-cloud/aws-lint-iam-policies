e9dcc69538fd3d805a5d5cf75a218103bdef88d0
michael-kirchner-at <michael.kirchner@welldone.cloud>
2025-08-01 08:36:53 +0200

    Fixed error messages for IAM Identity Center policies

--- modules/policy_types/iam_identity_center_application_policies.py
+++ modules/policy_types/iam_identity_center_application_policies.py
@@ -11,53 +11,62 @@ def analyze(account_id, region, boto_session, boto_config, policy_analysis_funct
 
     # Iterate all SSO instances
     instances_paginator = sso_client.get_paginator("list_instances")
-    for instances_page in instances_paginator.paginate():
-        for instance in instances_page["Instances"]:
+    try:
+        for instances_page in instances_paginator.paginate():
+            for instance in instances_page["Instances"]:
 
-            # Skip if this instance lives in a different AWS account, which happens when having SSO set up in the
-            # Organizations management account, but running this script in an Organizations member account.
-            if instance["OwnerAccountId"] != account_id:
-                continue
+                # Skip if this instance lives in a different AWS account, which happens when having SSO set up in the
+                # Organizations management account, but running this script in an Organizations member account.
+                if instance["OwnerAccountId"] != account_id:
+                    continue
 
-            # Iterate all applications of the instance
-            applications_paginator = sso_client.get_paginator("list_applications")
-            for applications_page in applications_paginator.paginate(InstanceArn=instance["InstanceArn"]):
-                for application in applications_page["Applications"]:
+                # Iterate all applications of the instance
+                applications_paginator = sso_client.get_paginator("list_applications")
+                for applications_page in applications_paginator.paginate(InstanceArn=instance["InstanceArn"]):
+                    for application in applications_page["Applications"]:
 
-                    # Skip if application is not associated with the account analyzed
-                    if "ApplicationAccount" in application and application["ApplicationAccount"] != account_id:
-                        continue
+                        # Skip if application is not associated with the account analyzed
+                        if "ApplicationAccount" in application and application["ApplicationAccount"] != account_id:
+                            continue
 
-                    # Iterate all application authentication methods
-                    try:
-                        authentication_methods_paginator = sso_client.get_paginator(
-                            "list_application_authentication_methods"
-                        )
-                        for authentication_methods_page in authentication_methods_paginator.paginate(
-                            ApplicationArn=application["ApplicationArn"]
-                        ):
-                            for authentication_method in authentication_methods_page["AuthenticationMethods"]:
+                        # Iterate all application authentication methods
+                        try:
+                            authentication_methods_paginator = sso_client.get_paginator(
+                                "list_application_authentication_methods"
+                            )
+                            for authentication_methods_page in authentication_methods_paginator.paginate(
+                                ApplicationArn=application["ApplicationArn"]
+                            ):
+                                for authentication_method in authentication_methods_page["AuthenticationMethods"]:
 
-                                # Skip if the authentication method is not IAM
-                                if authentication_method["AuthenticationMethodType"] != "IAM":
-                                    continue
+                                    # Skip if the authentication method is not IAM
+                                    if authentication_method["AuthenticationMethodType"] != "IAM":
+                                        continue
 
-                                policy = json.dumps(
-                                    authentication_method["AuthenticationMethod"]["Iam"]["ActorPolicy"]
-                                )
+                                    policy = json.dumps(
+                                        authentication_method["AuthenticationMethod"]["Iam"]["ActorPolicy"]
+                                    )
 
-                                policy_analysis_function(
-                                    account_id=account_id,
-                                    region=region,
-                                    source_service=SOURCE_SERVICE,
-                                    resource_type="AWS::SSO::Application",
-                                    resource_name=application["Name"],
-                                    resource_arn=application["ApplicationArn"],
-                                    policy_document=policy,
-                                    access_analyzer_type="RESOURCE_POLICY",
-                                    disabled_finding_issue_codes=["MISSING_RESOURCE", "INVALID_ACTION"],
-                                )
+                                    policy_analysis_function(
+                                        account_id=account_id,
+                                        region=region,
+                                        source_service=SOURCE_SERVICE,
+                                        resource_type="AWS::SSO::Application",
+                                        resource_name=application["Name"],
+                                        resource_arn=application["ApplicationArn"],
+                                        policy_document=policy,
+                                        access_analyzer_type="RESOURCE_POLICY",
+                                        disabled_finding_issue_codes=["MISSING_RESOURCE", "INVALID_ACTION"],
+                                    )
 
-                    except sso_client.exceptions.from_code("ValidationException"):
-                        # Skip if an application does not support authentication methods
-                        continue
+                        except sso_client.exceptions.from_code("ValidationException"):
+                            # Skip if an application does not support authentication methods
+                            continue
+
+    except sso_client.exceptions.from_code("AccessDeniedException") as ex:
+        # When certain SSO features are not available in a region, it will raise AccessDenied with a blank error
+        # message. If a caller is actually lacking permissions, there will be an error message set. Ignore cases
+        # where features are not available.
+        if not ex.response["Error"]["Message"].strip():
+            return
+        raise ex
--- modules/policy_types/iam_identity_center_permission_set_inline_policies.py
+++ modules/policy_types/iam_identity_center_permission_set_inline_policies.py
@@ -8,48 +8,57 @@ def analyze(account_id, region, boto_session, boto_config, policy_analysis_funct
 
     # Iterate all SSO instances
     instance_paginator = sso_client.get_paginator("list_instances")
-    for instances_page in instance_paginator.paginate():
-        for instance in instances_page["Instances"]:
+    try:
+        for instances_page in instance_paginator.paginate():
+            for instance in instances_page["Instances"]:
 
-            # Skip if this instance lives in a different AWS account, which happens when having SSO set up in the
-            # Organizations management account, but running this script in an Organizations member account.
-            if instance["OwnerAccountId"] != account_id:
-                continue
+                # Skip if this instance lives in a different AWS account, which happens when having SSO set up in the
+                # Organizations management account, but running this script in an Organizations member account.
+                if instance["OwnerAccountId"] != account_id:
+                    continue
 
-            # Iterate all permission sets
-            instance_arn = instance["InstanceArn"]
-            permission_set_paginator = sso_client.get_paginator("list_permission_sets")
-            try:
-                for permission_set_page in permission_set_paginator.paginate(InstanceArn=instance_arn):
-                    for permission_set_arn in permission_set_page["PermissionSets"]:
+                # Iterate all permission sets
+                instance_arn = instance["InstanceArn"]
+                permission_set_paginator = sso_client.get_paginator("list_permission_sets")
+                try:
+                    for permission_set_page in permission_set_paginator.paginate(InstanceArn=instance_arn):
+                        for permission_set_arn in permission_set_page["PermissionSets"]:
 
-                        # Get inline policy
-                        get_inline_policy_response = sso_client.get_inline_policy_for_permission_set(
-                            InstanceArn=instance_arn,
-                            PermissionSetArn=permission_set_arn,
-                        )
+                            # Get inline policy
+                            get_inline_policy_response = sso_client.get_inline_policy_for_permission_set(
+                                InstanceArn=instance_arn,
+                                PermissionSetArn=permission_set_arn,
+                            )
 
-                        # Skip if no inline policy is set
-                        if not get_inline_policy_response["InlinePolicy"]:
-                            continue
+                            # Skip if no inline policy is set
+                            if not get_inline_policy_response["InlinePolicy"]:
+                                continue
 
-                        # Get permission set details
-                        describe_permission_response = sso_client.describe_permission_set(
-                            InstanceArn=instance_arn,
-                            PermissionSetArn=permission_set_arn,
-                        )
+                            # Get permission set details
+                            describe_permission_response = sso_client.describe_permission_set(
+                                InstanceArn=instance_arn,
+                                PermissionSetArn=permission_set_arn,
+                            )
 
-                        policy_analysis_function(
-                            account_id=account_id,
-                            region=region,
-                            source_service=SOURCE_SERVICE,
-                            resource_type="AWS::SSO::PermissionSet",
-                            resource_name=describe_permission_response["PermissionSet"]["Name"],
-                            resource_arn=permission_set_arn,
-                            policy_document=get_inline_policy_response["InlinePolicy"],
-                            access_analyzer_type="IDENTITY_POLICY",
-                        )
+                            policy_analysis_function(
+                                account_id=account_id,
+                                region=region,
+                                source_service=SOURCE_SERVICE,
+                                resource_type="AWS::SSO::PermissionSet",
+                                resource_name=describe_permission_response["PermissionSet"]["Name"],
+                                resource_arn=permission_set_arn,
+                                policy_document=get_inline_policy_response["InlinePolicy"],
+                                access_analyzer_type="IDENTITY_POLICY",
+                            )
 
-            except sso_client.exceptions.from_code("ValidationException"):
-                # Account instances of Identity Center don't support permission sets, thus continue here
-                continue
+                except sso_client.exceptions.from_code("ValidationException"):
+                    # Account instances of Identity Center don't support permission sets, thus continue here
+                    continue
+
+    except sso_client.exceptions.from_code("AccessDeniedException") as ex:
+        # When certain SSO features are not available in a region, it will raise AccessDenied with a blank error
+        # message. If a caller is actually lacking permissions, there will be an error message set. Ignore cases
+        # where features are not available.
+        if not ex.response["Error"]["Message"].strip():
+            return
+        raise ex
